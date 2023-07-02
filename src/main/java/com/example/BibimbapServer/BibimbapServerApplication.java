0113316c3419245689b3b9d95aaa5bc80a3db781
cjsqudwns <jd8795@naver.com>
2023-07-02 09:53:19 +0900

    doing: Init

---
+++ src/main/java/com/example/BibimbapServer/BibimbapServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.BibimbapServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibimbapServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibimbapServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BibimbapServer/BibimbapServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BibimbapServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibimbapServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
